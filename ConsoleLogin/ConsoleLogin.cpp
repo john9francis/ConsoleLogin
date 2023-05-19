e1acf6aacbb189e922fcaeec5d17fee92c6d1cc5
John Francis <john9francis@gmail.com>
2023-05-19 18:56:12 -0400

    created a basic user interface in the console

--- ConsoleLogin/ConsoleLogin.cpp
+++ ConsoleLogin/ConsoleLogin.cpp
@@ -2,8 +2,7 @@
 
 #include <iostream>
 
-// we need to include any classes that 
-we use.
+// we need to include any classes that we use.
 
 #include "database.h"
 #include "account.h"
@@ -14,12 +13,36 @@ we use.
 
 int main()
 {
-	// main program:
-	
+	// create the database:
 	Database accountDatabase;
-	accountDatabase.AddAccount();
-	accountDatabase.AddAccount();
-	accountDatabase.DisplayDatabase();
+	bool x = true;
+
+	while (x) {
+
+		// get user input
+		std::string input;
+		std::cout << "Welcome. Please choose an option." << std::endl;
+		std::cout << "To sign in, press 1." << std::endl;
+		std::cout << "To create an account, press 2." << std::endl;
+		std::cout << "To quit, Press 3." << std::endl;
+		std::cin >> input;
+
+		// do what the user wants
+		if (std::stoi(input) == 1) {
+			std::cout << "you chose 1." << std::endl;
+		}
+		else if (std::stoi(input) == 2) {
+			std::cout << "you chose 2." << std::endl;
+		}
+		else if (std::stoi(input) == 3) {
+			std::cout << "quitting now... " << std::endl;
+			x = false;
+		}
+		else {
+			std::cout << "sorry, I didn't recognize that response. ";
+			std::cout << "please input 1, 2, or 3 and press enter. " << std::endl;
+		}
+	}
 
 	//system("pause") is just makes it so the console doesn't close immediately.
 	
