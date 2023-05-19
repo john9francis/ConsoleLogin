c615ce0c8ba7759b67149a9737effaf27b9d6c55
John Francis <john9francis@gmail.com>
2023-05-19 14:37:26 -0400

    added the header files and resource files to their correct folders, that way I don't need to include the cpp files in the main program file, I can just include the header files.

--- ConsoleLogin/ConsoleLogin.cpp
+++ ConsoleLogin/ConsoleLogin.cpp
@@ -4,11 +4,17 @@
 
 // we need to include any classes that we use. Specifically the cpp files.
 
+/*
 #include "account.cpp"
 #include "database.cpp"
 #include "numberAccount.cpp"
 #include "securityAccount.cpp"
+*/
 
+#include "database.h"
+#include "account.h"
+#include "numberAccount.h"
+#include "securityAccount.h"
 
 // main is an int because it returns 0 if it's successful
 
@@ -16,15 +22,15 @@ int main()
 {
 	// main program:
 
-	/*
+	
 	Database accountDatabase;
 	accountDatabase.AddAccount();
 	accountDatabase.AddAccount();
 	accountDatabase.DisplayDatabase();
-	*/
+	
 
 	//testing
-
+	/*
 	Account* n = new NumberAccount();
 	n->CreateAccount();
 	n->DisplayAccount();
@@ -39,6 +45,7 @@ int main()
 	s->CreateAccount();
 	s->DisplayAccount();
 	delete s;
+	*/
 
 	//system("pause") is just makes it so the console doesn't close immediately.
 	
